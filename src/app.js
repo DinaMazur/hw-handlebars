961afa8d6ba8dd7e4d93bc7747323bcd59ea4630
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2025-05-10 15:27:16 +0300

    finish-fix

--- src/app.js
+++ src/app.js
@@ -1,20 +1,6 @@
 import {products} from "./data/handlbares.json"
-import createList from "./templates/handlbares-templates.hbs"
-export const products= [{
-    "id": 1,
-    "name": "Laptop",
-    "price": 1500,
-    "description": "A high-performance laptop for all your needs."
-  },
-  {
-    "id": 2,
-    "name": "Smartphone",
-    "price": 700,
-    "description": "A modern smartphone with an excellent camera."
-  },
-  {
-    "id": 3,
-    "name": "Headphones",
-    "price": 200,
-    "description": "Noise-cancelling headphones for better focus."
-  }]
+import createList from "./templates/handlebars-templates.hbs"
+const result = createList({products});
+document.querySelector(".box").innerHTML = result;
+console.log(result)
+
